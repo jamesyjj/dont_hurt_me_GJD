05e5e7e137d5dd8cc9c6eedd7c9a7d345a0aee22
laojue <1317609355@qq.com>
2026-06-16 23:35:20 +0800

    - add 获取fred数据接口;

---
+++ src/macro_economy/readme.md
@@ -0,0 +1,19 @@
+FRED 美国圣路易斯联储官方数据库
+
+https://fred.stlouisfed.org/series/CPIAUCSL
+
+
+
+- CPI
+	- 对应代码：CPIAUCSL
+- 核心CPI
+	- CPILFESL
+- 联邦基金利率
+	- FEDFUNDS
+- 失业率
+	- UNRATE
+- M2
+	- M2SL
+- 10年美债收益率
+	- DGS10
+- 
--- src/macro_economy/usa.py
+++ src/macro_economy/usa.py
@@ -1,10 +1,41 @@
 import akshare as ak
+import pandas as pd
 
 def get_gdp():
     macro_usa_gdp_monthly_df = ak.macro_usa_gdp_monthly()
     print(macro_usa_gdp_monthly_df)
 
+# 获取fred数据
+# req: 所需数据全称
+# 全程列表如下：CPIAUCSL，UNRATE，FEDFUNDS（待补充）
+
+def get_fred(series_id):
+    url = (
+        "https://fred.stlouisfed.org/graph/fredgraph.csv"
+        f"?id={series_id}"
+    )
+    df = pd.read_csv(url)
+    print(df.tail())
+    print("总条数:", len(df))
+    print("最早日期:", df.iloc[0])
+    print("最新日期:", df.iloc[-1])
+
+
+
+def get_cpi_from_fred():
+    url = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=CPIAUCSL"
+    df = pd.read_csv(url)
+    print(df.tail())
+    print("总条数:", len(df))
+    print("最早日期:", df.iloc[0])
+    print("最新日期:", df.iloc[-1])
+
 
 
 if __name__ == '__main__':
-    get_gdp()
+    # get_gdp()
+    # get_cpi_from_fred()
+
+    cpi = get_fred("CPIAUCSL")
+    unrate = get_fred("UNRATE")
+    fedfunds = get_fred("FEDFUNDS")
